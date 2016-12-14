9a2b324c13ea99e5a66a7312f4e380793786a450
Ian Li <ianli6362@gmail.com>
2016-12-13 18:30:19 -0800

    change User Modal

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'imagepath','longtitude','latitude','aboutus','address','city','state','zipcode'
     ];
 
     /**
