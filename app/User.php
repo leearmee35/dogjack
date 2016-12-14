a5edbfc8687a99b1c0506e667486c099b5c61ae7
Ian Li <ianli6362@gmail.com>
2016-12-13 18:35:59 -0800

    change User Modal

--- app/User.php
+++ app/User.php
@@ -18,6 +18,10 @@ class User extends Authenticatable
         'name', 'email', 'password', 'imagepath','longtitude','latitude','aboutus','address','city','state','zipcode'
     ];
 
+    protected $attributes = array(
+        'aboutus' => 'All about that bass',
+    );
+
     /**
      * The attributes that should be hidden for arrays.
      *
