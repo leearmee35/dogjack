9c4fc666b778c3e3094cccebf49fd9e9e6362201
Ian Li <ianli6362@gmail.com>
2016-12-13 18:18:56 -0800

    add more to User model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('imagepath');
+            $table->double('longtitude');
+            $table->double('latitude');
+            $table->string('aboutus');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
