43e6acd2c1c0fa962ade2585a9e35757dc470f0a
Ian Li <ianli6362@gmail.com>
2016-12-18 07:04:00 -0800

    say something

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -25,29 +25,34 @@ class EmployerController extends Controller
 
     public function storeProfile(Request $request){
 
+        $name = $request->input('name');
+        $imagepath = $name;
+        $longtitude = $request->input('longtitude');
+        $latitude = $request->input('latitude');
+        $aboutus = $request->input('aboutus');
+        $user = User::find($request->input('id'));
+
+        if(!$request->input('name')){
+            $name = "Defaut Name";
+        }
+
         if(Input::hasFile('image')){
 
 
-            $img = Image::make(Input::file('image'))->resize(300, 400)->save('images/abc.jpg');
+            $img = Image::make(Input::file('image'))->resize(300, 400)->save('images/'.$name.'.jpg');
 
-            $name = $request->input('name');
-            $imagepath = $name;
-            $longtitude = $request->input('longtitude');
-            $latitude = $request->input('latitude');
-            $aboutus = $request->input('aboutus');
-            $user = User::find($request->input('id'));
-
-            $user->name = $name;
-            $user->imagepath = $imagepath;
-            $user->longtitude = $longtitude;
-            $user->latitude = $latitude;
-            $user->aboutus = $aboutus;
-            $user->save();
-
-            return $user;
         }
 
-        return "No";
+
+
+        $user->name = $name;
+        $user->imagepath = $imagepath;
+        $user->longtitude = $longtitude;
+        $user->latitude = $latitude;
+        $user->aboutus = $aboutus;
+        $user->save();
+
+        return response()->json(compact('user'));
     }
 
 }
